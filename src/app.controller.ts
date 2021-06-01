27c7ce88dc89fe45ae4d87408bb227702d5586e2
hamidehalaee <hamidehalaeee@gmail.com>
2021-06-01 16:58:26 +0430

    it works

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,66 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import {Controller, Get, Render, Body, Post, Req, Res} from '@nestjs/common';
 import { AppService } from './app.service';
+import * as speakeasy from 'speakeasy';
+import {spawn} from "child_process";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Get('secret')
+  async secret(){
 
-  @Get()
-  @Render('index')
-  getQRcode(): any {
-    return this.appService.getQRcode();
+
+    let secret = speakeasy.generateSecret({length: 20});
+    console.log(secret)
+    return `"secret": ${secret.base32}`
   }
 
+  // @Post('generate')
+  // async generate(@Body() body) {
+  //   let token = speakeasy.totp({
+  //     secret: body.secret,
+  //     encoding: "base32"
+  //   })
+  //   return `"token":${token}`;
+  // }
+
+  @Post('validate')
+  async validate (@Body() body){
+    let validate = speakeasy.totp.verify({
+      secret: body.secret,
+      encoding: "base32",
+      token: body.token,
+      window: 1
+    })
+    return `"valid": ${validate}`
+  }
+
+
+
   // @Get()
-  // verify(): any {
-  //   return this.appService.verify();
+  // @Render('index')
+  // async GetAll(@Res()res,@Body()body) {
+  //   console.log(res,body)
+  // }
+  //
+  // @Post()
+  // async PostAll(@Body() body){
+  //   //BODY.TOKEN => THE CODE YOU PUT ON
+  //   // return this.appService.GetQRCode(body.token);
+  // }
+  //
+  // @Post('register')
+  // async RegisterUser(@Body() body,@Res() res){
+  //   return this.appService.RegisterUser(res);
+  // }
+  //
+  // @Post('verify')
+  // async verify(@Body() body,@Res() res){
+  //   return this.appService.verify(body.userId , body.token, res )
+  // }
+  //
+  // @Post('validate')
+  // async validate(@Body() body ,@Res() res){
+  //   return this.appService.validate(body.userId , body.token , res)
   // }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,39 +1,92 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, Res} from '@nestjs/common';
 import * as speakeasy from 'speakeasy';
 import * as qrcode from 'qrcode';
+import * as fetch from 'node-fetch';
+import { JsonDB } from 'node-json-db';
+import { Config } from 'node-json-db/dist/lib/JsonDBConfig';
+import * as uuid from 'uuid';
+// const db = new JsonDB(new Config("MyDB",true,false,"/"))
 @Injectable()
 export class AppService {
 
-  async getQRcode() {
-    const secret = speakeasy.generateSecret({
-      name: 'citex'
-    })
-    console.log(secret)
-    // {
-    //   ascii: 'bD*b*no5P&cTp%}T[yz&.zbgMPiZa<S1',
-    //   hex: '62442a622a6e6f355026635470257d545b797a262e7a62674d50695a613c5331',
-    //   base32: 'MJCCUYRKNZXTKUBGMNKHAJL5KRNXS6RGFZ5GEZ2NKBUVUYJ4KMYQ',
-    //   otpauth_url: 'otpauth://totp/citex?secret=MJCCUYRKNZXTKUBGMNKHAJL5KRNXS6RGFZ5GEZ2NKBUVUYJ4KMYQ'
-    // }
-    let QRcode = []; 
-    await qrcode.toDataURL(secret.otpauth_url,async(err,data) => {
-      // QRcode.push(data)
-      // write('<img src="' + data + '">');
-      console.log(data);
-      
-    })
-    
 
-    console.log(QRcode)
-    
-    var verified = speakeasy.totp.verify({
-      secret: 'Z627h3ZO^mKi}d@O#dHW42Zmu]M!$i(/',
-      encoding: 'ascii',
-      token: '080341'
-    })
-    console.log(verified)
-  }
-
-  // async verify() {
+  // async RegisterUser(@Res()res){
+  //   //CREATE DATABASE
+  //   const db = new JsonDB(new Config("MyDB",true,false,"/"))
+  //   //REGISTER USER
+  //   const id = uuid.v4()
+  //   try{
+  //     const path = `/user/${id}`
+  //     const temp_secret = speakeasy.generateSecret({name:'Citex'})
+  //     db.push(path,{id, temp_secret})
+  //     res.json( {
+  //       "id": id,
+  //       "secret": temp_secret.base32
+  //     })
+  //   }
+  //   catch (error){
+  //     console.log(error)
+  //     // res.status(500).json({ message: `Error generating secret key-${error}`})
+  //   }
+  // }
+  // async verify(userId,token,@Res()res){
+  //   // var secret = ...
+  //   //
+  //   // var token = speakeasy.totp({
+  //   //   secret: secret,
+  //   //   encoding: 'base32'
+  //   // });
+  //   const db = new JsonDB(new Config("MyDB",true,false,"/"))
+  //   try {
+  //     // Retrieve user from database
+  //     const path = `/user/${userId}`;
+  //     const user = db.getData(path);
+  //     console.log({ user })
+  //     const { base32: secret } = user.temp_secret;
+  //     const verified = speakeasy.totp.verify({
+  //       secret,
+  //       encoding: 'base32',
+  //       token
+  //     });
+  //     if (verified) {
+  //       // Update user data
+  //       db.push(path, { id: userId, secret: user.temp_secret });
+  //       // res.json({ verified: true })
+  //       return "false"
+  //     } else {
+  //       // res.json({ verified: false})
+  //       return "false"
+  //     }
+  //   } catch(error) {
+  //     console.error(error);
+  //     // res.status(500).json({ message: 'Error retrieving user'})
+  //   };
+  // }
+  // async validate(userId,token,@Res()res){
+  //   const db = new JsonDB(new Config("MyDB",true,false,"/"))
+  //   try {
+  //     // Retrieve user from database
+  //     const path = `/user/${userId}`;
+  //     const user = db.getData(path);
+  //     console.log({ user })
+  //     const { base32: secret } = user.secret;
+  //     // Returns true if the token matches
+  //     const tokenValidates = speakeasy.totp.verify({
+  //       secret,
+  //       encoding: 'base32',
+  //       token,
+  //       window: 1
+  //     });
+  //     if (tokenValidates) {
+  //       // res.json({ validated: true })
+  //       return "true"
+  //     } else {
+  //       // res.json({ validated: false})
+  //       return "false"
+  //     }
+  //   } catch(error) {
+  //     console.error(error);
+  //     // res.status(500).json({ message: 'Error retrieving user'})
+  //   };
   // }
 }
